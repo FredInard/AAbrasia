61e3286a4d8901e510d3e3565d5aedf71aa25483
Dagets <inard.frederic@gmail.com>
2023-08-01 17:50:22 +0200

    configuration backend front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+require("dotenv").config();
+import Home from "./pages/Home"
+
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <p>coucou</p>
+    </div>
+  )
+}
+
+export default App
