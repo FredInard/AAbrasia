c1e82b37d51760ef5bacb5249788572d858b749e
Dagets <inard.frederic@gmail.com>
2023-09-11 23:03:01 +0200

    ok

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,50 +25,95 @@ export default function Home() {
       </div>
 
       <div className="ContaineurBanner">
-        <img src={BrushUp} alt="brush haut pour fondue image de scene" />
+        <img
+          src={BrushUp}
+          alt="brush haut pour fondue image de scene"
+          className="superpose haut"
+        />
         <img
           src={scene}
-          alt="scene de jeux de role avec des personnages de type héroîc fantaisie autour d'une table."
+          alt="scene de jeux de role avec des personnages de type héroïc fantasy autour d'une table."
+          className="scene"
         />
-        <img src={BrushDown} alt="brush bas pour fondue image de scene" />
+        <img
+          src={BrushDown}
+          alt="brush bas pour fondue image de scene"
+          className="superpose bas"
+        />
       </div>
 
-      <div className="ContaineurPrésentation">
+      <div className="containeurPresentation">
         <div>
-          <div>
+          <div className="titreh2">
             <h2>L’association de jeux de rôle du 04</h2>
           </div>
-          <img src={wizard} alt="image d'un magicien type Dnd" />
+          <div className="boxMage">
+            <div className="boxIamgeMage">
+              <img src={wizard} alt="image d'un magicien type Dnd" />
+            </div>
+            <div className="boxTextMage fade-in-right">
+              <p>
+                {" "}
+                Bienvenue, <br />
+                Les Arpenteurs d’Abrasia sont une association qui organise des
+                parties de jeu de rôle sur table. Nous jouons principalement
+                (mais pas que) dans un univers fantastique nommé Abrasia, et
+                tout le monde est bienvenu·e, néophytes ou vétérans, pour créer
+                des moments conviviaux de rires et d’aventures !
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="boxWarior">
+          <div className="boxTextWarior">
+            <p>
+              C’est quoi le jeux de rôle ? Le jeu de rôle est un loisir qui se
+              pratique en petits groupes (entre 3 et 6 personnes) autour d'une
+              table. Une personne prend le rôle de meneuse de jeu et raconte une
+              histoire dans laquelle chacune des personnes attablées incarne un
+              personnage. Le but est d’imaginer et mettre en place
+              collectivement des solutions pour déjouer les pièges et mener à
+              bien l’aventure... A conditions que les dés le permettent !
+            </p>
+          </div>
+          <div className="boximageWarior">
+            <img src={questioningFemale} alt="image d'une guerrière type Dnd" />
+          </div>
+        </div>
+        <div className="boxCitadel">
+          <div className="boxImageCitadel">
+            <img src={Citadel} alt="image de la citadel de Sisteton" />
+          </div>
+          <div className="boxTextCitadel">
+            <p>
+              Présentation de l’asso L'association des Arpenteurs d'Abrasia (AA)
+              a une double vocation ludique et culturelle. Elle a pour objet de
+              réunir des joueurs et joueuses de façon régulière autour de tables
+              de jeu de rôle (jdr) afin de partager des moments conviviaux et
+              inclusifs.
+            </p>
+          </div>
+        </div>
+        <div className="boxTextCitadelBis">
           <p>
-            {" "}
-            Bienvenue, Les Arpenteurs d’Abrasia sont une association qui
-            organise des parties de jeu de rôle sur table. Nous jouons
-            principalement (mais pas que) dans un univers fantastique nommé
-            Abrasia, et tout le monde est bienvenu·e, néophytes ou vétérans,
-            pour créer des moments conviviaux de rires et d’aventures !
+            Elle se donne pour mission de créer du lien social, d'une part en
+            créant des espaces de jeu mixtes et bienveillants pour toute
+            personne souhaitant la rejoindre, d'autre part en allant vers des
+            publics en recherche de sociabilités et/ou isolées, telles que les
+            personnes retraitées ou les adolescents. L'association entend
+            développer un volet culturel en organisant des ateliers d'écriture
+            comme un complément des activités de jeu de rôle, des groupes de
+            lecture, voire d'autres activités liées à la production de
+            l'imaginaire.
           </p>
         </div>
-        <div>
-          <p>
-            C’est quoi le jeux de rôle ? Le jeu de rôle est un loisir qui se
-            pratique en petits groupes (entre 3 et 6 personnes) autour d'une
-            table. Une personne prend le rôle de meneuse de jeu et raconte une
-            histoire dans laquelle chacune des personnes attablées incarne un
-            personnage. Le but est d’imaginer et mettre en place collectivement
-            des solutions pour déjouer les pièges et mener à bien l’aventure...
-            A conditions que les dés le permettent !
-          </p>
-          <img src={questioningFemale} alt="image d'une guerrière type Dnd" />
+      </div>
+      <div className="agendaDesParties">
+        <div className="titreAPartie">
+          <h2> Agenda des parties</h2>
         </div>
         <div>
-          <img src={Citadel} alt="image de la citadel de Sisteton" />
-          <p>
-            Présentation de l’asso L'association des Arpenteurs d'Abrasia (AA) a
-            une double vocation ludique et culturelle. Elle a pour objet de
-            réunir des joueurs et joueuses de façon régulière autour de tables
-            de jeu de rôle (jdr) afin de partager des moments conviviaux et
-            inclusifs.
-          </p>
+          <h2> Game carde </h2>
         </div>
       </div>
     </>
