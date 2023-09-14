dae6009db0bbf5680b765b1b00f52cd6beb25fe7
Dagets <inard.frederic@gmail.com>
2023-09-15 01:00:18 +0200

    navbar in progress

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,46 +1,92 @@
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-
 import logo from "../assets/pics/logo.png"
-
 import "./NavBar.scss"
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsMobile(true)
+      } else {
+        setIsMobile(false)
+      }
+    }
+
+    window.addEventListener("resize", handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-    <>
-      <div className="navBarDesktopContainer">
-        <div className="boxlogoNavBar">
-          <img
-            className="logoNavBar"
-            src={logo}
-            alt="logo of website in the navbar"
-          />
+    <div className="navBarDesktopContainer header">
+      <div className="boxlogoNavBar">
+        <img
+          className="logoNavBar logo"
+          src={logo}
+          alt="logo of website in the navbar"
+        />
+      </div>
+      {isMobile && (
+        <button className="menuButton" onClick={toggleMenu}>
+          {isMenuOpen ? "Fermer" : "Menu"}
+        </button>
+      )}
+      {isMenuOpen && isMobile && (
+        <div className="menuItems">
+          <Link className="menuItem menuItem-2" to="/">
+            Home
+          </Link>
+          <Link className="menuItem menuItem-2" to="/Inscription">
+            Inscription
+          </Link>
+          <Link className="menuItem menuItem-2" to="/profil">
+            Profil
+          </Link>
+          <Link className="menuItem menuItem-2" to="/create-game">
+            Créer ta partie
+          </Link>
+          <Link className="menuItem menuItem-2" to="/Association">
+            L'Association
+          </Link>
+          <Link className="menuItem menuItem-2" to="/Creation">
+            Création
+          </Link>
         </div>
+      )}
+      {!isMobile && (
         <div className="mainButtonsNavBar">
-          <Link className="buttonNavBar" to="/">
+          <Link className="buttonNavBar nav-item" to="/">
             <p>HOME</p>
           </Link>
-          <Link className="buttonNavBar" to="/Inscription">
+          <Link className="buttonNavBar nav-item" to="/Inscription">
             <p>Inscription</p>
           </Link>
-          <Link className="buttonNavBar" to="/profil">
+          <Link className="buttonNavBar nav-item" to="/profil">
             <p>Profil</p>
           </Link>
-          <Link className="buttonNavBar" to="/create-game">
+          <Link className="buttonNavBar nav-item" to="/create-game">
             <p>Créer ta partie</p>
           </Link>
-          <Link className="buttonNavBar" to="/Association">
-            <p>L'Assocation</p>
+          <Link className="buttonNavBar nav-item" to="/Association">
+            <p>L'Association</p>
           </Link>
-          <Link className="buttonNavBar" to="/Creation">
+          <Link className="buttonNavBar nav-item" to="/Creation">
             <p>Création</p>
           </Link>
-          {/* <p>Agenda des parties</p>
-                <Link to="/Creation">
-                    <p>LOG OUT</p>
-                </Link>     */}
         </div>
-      </div>
-    </>
+      )}
+    </div>
   )
 }
 
